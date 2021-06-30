07920df867a7624a3b3227ff3f0e1977cf5ff84a
Maneesh k <77183545+Maneesh-k@users.noreply.github.com>
2021-06-30 14:42:11 +0530

    update packages and app.js

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const jsonServer = require('json-server');
+const express = require('express');
 
-const server = jsonServer.create();
-const router = jsonServer.router('data.json');
+const app = express();
+const router = express.Router();
 
-const middlewares = jsonServer.defaults();
+const redirectTO = (req, res) => {
+  res.redirect('https://node-task-deploy.herokuapp.com/api/v1/storedata', 302);
+};
+
+const redirect = router.get('', redirectTO);
+
+app.use('/', redirect);
+
 const port = process.env.PORT || 8000;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
